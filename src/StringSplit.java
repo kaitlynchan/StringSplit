5570be4635d576dba6b6a06abbf6c91bbb75b7f3
Kaitlyn Chan <ununoctium829@gmail.com>
2016-10-23 12:23:16 -0700

    sandwich stuff

--- src/StringSplit.java
+++ src/StringSplit.java
@@ -1,19 +1,30 @@
 
-public class Split 
+public class StringSplit 
 {
 
 	public static void main(String[] args) 
 	{
+		String bread = "apples pineapples bread lettus tomato bacon mayo ham bread cheese";
+		
+		
+		String [] ingredients = bread.split("bread");
+		int slices = ingredients.length-1;
+		for (int i = 0; i < ingredients.length; i++){
+			System.out.println(ingredients[i]);
+		}
+		
+		
+		
 		//String.split();
 		//It's a method that acts on a string, <StringName>.split(<String sp>);
 		//Where sp is the string where the string splits
 		//And it returns an array
 		// Example: "I like apples!".split(" "); 
 		//		it will split at spaces and return an array of ["I","like","apples!"]
-		// Example 2: "I really like really red apples"split("really")
-		//		it will split at the word "really" and return an array of ["I "," like "," apples!"]
+		// Example 2: "I really like really red apples".split("really")
+		//		it will split at the word "really" and return an array of ["I "," like "," red apples!"]
 		
-		//play around with String.split! what happens if you "I reallyreally like apples".split("really") ?
+		//play around with String.split! what happens if you "I really really like apples".split("really") ?
 		
 		
 		//Your task:
