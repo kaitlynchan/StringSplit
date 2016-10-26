b5beca42fa6e33ce4d239fbedcde2fe8a04f98a4
Kaitlyn Chan <ununoctium829@gmail.com>
2016-10-25 22:49:18 -0700

    all done

--- src/StringSplit.java
+++ src/StringSplit.java
@@ -1,17 +1,88 @@
+import java.util.*;
+
+//Kaitlyn Chan
+//APCS Period 1
+// October 25, 2016
+//String Split finds all the ingredients inside a sandwich (between two slices of bread) in order to before the ultimate chef.
 
 public class StringSplit 
 {
 
 	public static void main(String[] args) 
 	{
-		String bread = "apples pineapples bread lettus tomato bacon mayo ham bread cheese";
+		//prints the ingredients between the slices of bread in a sandwich
+		split("bloodsweattearsbreadapplesbreadlettusbreadtomatobaconmayohambreadcheese");
+		//prints the ingredients between the slices of bread in a spaced sandwich 
+		splitSpaces("apples pineapples bread lettus tomato bacon mayo ham bread cheese");
 		
-		
-		String [] ingredients = bread.split("bread");
+	}
 	
-		for (int i = 0; i < ingredients.length; i++){
-			System.out.println(ingredients[i]);
+	public static void split (String sandwich){
+		
+		//if there is no bread (carb free!)
+		if (sandwich.indexOf("bread")<0){
+			System.out.println("not a sandwich!");
 		}
+		
+		//if there is only one slice of bread (open face sandwich!)
+		//cut out string after first bread
+		String bread = sandwich.substring(sandwich.indexOf("bread")+5);
+		if (bread.indexOf("bread")<0){
+			System.out.println("not a real sandwich!");
+		}
+		
+		//we know that there is at least one slice of bread
+		int slices = 1;
+		//while there is still bread in the string, eliminate each slice 
+		while (bread.indexOf("bread") >= 0){
+			bread = bread.substring(bread.indexOf("bread")+5);
+			slices++;
+			
+		}
+		System.out.println("Slices of bread: " + slices);
+		
+		//take out all the bread and place ingredients into an array
+		String [] ingredients = sandwich.split("bread");
+		
+		
+		//if sandwich has stuff before first bread and ends in bread take all ingredients after the first element
+		if ((sandwich.indexOf("bread")>0) && (bread.equals("")) ){
+			for (int i = 1; i < ingredients.length; i++){
+				System.out.print(ingredients[i]);
+			}
+		}
+		
+		//if sandwich begins in bread and ends in bread (like a normal sandwich) take all ingredients
+		else if (bread.equals("")){
+			for (int i = 0; i < ingredients.length; i++){
+				System.out.print(ingredients[i]);
+			}
+		}
+		
+		//if a sandwich has stuff before and after the boundary slices take all ingredients between first and last element 
+		else if (sandwich.indexOf("bread")>0){
+			for (int i = 1; i < ingredients.length-1; i++){
+				System.out.print(ingredients[i]);
+			}
+		}
+		
+		//if a sandwich begins in bread and has stuff after the last slice take all ingredients before last element 
+		else {
+			for (int i = 0; i < ingredients.length-1; i++){
+				System.out.print(ingredients[i]);
+			}
+		}
+		System.out.println();	
+		
+	}
+	
+	public static void splitSpaces (String sandwich){
+		//take out the spaces in the sandwich string
+		String [] nospacesandwich = sandwich.split("  ");
+		
+		//run the split method on the unspaced sandwich string
+		split(Arrays.toString(nospacesandwich));
+	}
 		
 		
 		
@@ -44,4 +115,4 @@ public class StringSplit
 
 	}
 
-}
+
